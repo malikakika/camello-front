3d322a3b62585523f2536ed72bd7b067b9f2d18f
malikakika <malikachoubri@gmail.com>
2023-05-23 17:35:46 +0100

    last commit

--- src/app/pages/request-page/request-page.component.ts
+++ src/app/pages/request-page/request-page.component.ts
@@ -10,22 +10,12 @@ export class RequestPageComponent {
 
   showFormMessage: boolean = false;
   showFormItem: boolean = false;
-  showFormContact: boolean = false;
 
-  contactForm: FormGroup;
   messageForm: FormGroup;
   itemForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
-    this.contactForm = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      address: ['', Validators.required],
-      city: ['', Validators.required],
-      postalCode: ['', Validators.required],
-      country: ['', Validators.required]
-    });
+
 
     this.messageForm = this.formBuilder.group({
       subject: ['', Validators.required],
@@ -39,18 +29,7 @@ export class RequestPageComponent {
     });
   }
 
-  showContactForm() {
-    this.showFormContact = !this.showFormContact;
 
-    // Réinitialiser le formulaire lorsque vous le masquez
-    if (!this.showFormContact) {
-      this.contactForm.reset();
-    }
-
-    // Masquer les autres formulaires
-    this.showFormMessage = false;
-    this.showFormItem = false;
-  }
 
   showMessageForm() {
     this.showFormMessage = !this.showFormMessage;
@@ -61,7 +40,6 @@ export class RequestPageComponent {
     }
 
     // Masquer les autres formulaires
-    this.showFormContact = false;
     this.showFormItem = false;
   }
 
@@ -74,31 +52,27 @@ export class RequestPageComponent {
     }
 
     // Masquer les autres formulaires
-    this.showFormContact = false;
     this.showFormMessage = false;
   }
 
   onSubmit() {
     // Vérifier la validité de chaque formulaire individuellement
-    if (this.contactForm.invalid || this.messageForm.invalid || this.itemForm.invalid) {
+    if (this.messageForm.invalid || this.itemForm.invalid) {
       return;
     }
 
     // Envoyer les données du formulaire
-    console.log('Contact Form Data:', this.contactForm.value);
     console.log('Message Form Data:', this.messageForm.value);
     console.log('Item Form Data:', this.itemForm.value);
   }
 
   submitAllForms() {
     // Vérifier la validité de tous les formulaires
-    if (this.contactForm.valid && this.messageForm.valid && this.itemForm.valid) {
-      const contactFormData = this.contactForm.value;
+    if (this.messageForm.valid && this.itemForm.valid) {
       const messageFormData = this.messageForm.value;
       const itemFormData = this.itemForm.value;
 
       // Envoyer les données à votre backend ou effectuer une action appropriée
-      console.log('Contact Form Data:', contactFormData);
       console.log('Message Form Data:', messageFormData);
       console.log('Item Form Data:', itemFormData);
     } else {
